d4ad75d36b9b43a12f813732778f3f6ca2693a70
szuck <szuck@Kacper.localdomain>
2023-10-23 18:55:07 +0200

    Committing as second project just because previous one was done on another computer. In this commit I added API for uploading and downloading files, it works good (checked in postman), however it's not compatible while using thymeleaf so I decided to make a new controller, and now I'm working on it

---
+++ src/main/java/pl/kszucki/crmznsurance/Service/PolicyService.java
@@ -0,0 +1,30 @@
+package pl.kszucki.crmznsurance.Service;
+
+import pl.kszucki.crmznsurance.Entity.InsPolicy;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
+public interface PolicyService {
+    List<InsPolicy> getAllPolicies();
+
+    InsPolicy savePolicy(InsPolicy insPolicy);
+
+    List<InsPolicy> getAllByUserId(Long Id);
+
+    InsPolicy getPolicyById(Long id);
+
+    InsPolicy updatePolicy(InsPolicy policy);
+
+    void deletePolicy(Long id);
+
+    List<InsPolicy> search(String query);
+
+    List<InsPolicy> getPolicyBetweenDate(Date startDate, Date endDate);
+
+    List<InsPolicy> searchPoliciesByDate(Date startDate, Date endDate);
+
+    public List<InsPolicy> searchByDateRange(String startDate, String endDate);
+
+}
